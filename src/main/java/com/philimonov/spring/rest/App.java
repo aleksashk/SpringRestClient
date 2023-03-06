8c0892122e195e10a8041c0be503f851fdd668d0
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-03-06 18:40:30 +0300

    add new Employee to DB

--- src/main/java/com/philimonov/spring/rest/App.java
+++ src/main/java/com/philimonov/spring/rest/App.java
@@ -14,7 +14,13 @@ public class App {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
 
         Communication communication = context.getBean("communication", Communication.class);
-        List<Employee> allEmployees = communication.getAllEmployees();
-        System.out.println(allEmployees);
+//        List<Employee> allEmployees = communication.getAllEmployees();
+//        System.out.println(allEmployees);
+
+//        Employee employee = communication.getEmployee(1);
+//        System.out.println(employee);
+
+        Employee employee = new Employee("Sveta", "Sokolova", "HR", 800);
+        communication.saveEmployee(employee);
     }
 }
--- src/main/java/com/philimonov/spring/rest/Communication.java
+++ src/main/java/com/philimonov/spring/rest/Communication.java
@@ -27,11 +27,23 @@ public class Communication {
     }
 
     public Employee getEmployee(int id) {
-        return null;
+
+        Employee employee = restTemplate.getForObject(URL + "/" + id, Employee.class);
+        return employee;
     }
 
-    public void saveEmployee(int id) {
+    public void saveEmployee(Employee employee) {
 
+        int id = employee.getId();
+        if (id == 0) {
+            ResponseEntity<String> responseEntity = restTemplate.postForEntity(URL, employee, String.class);
+
+            System.out.println("New employee was added to DB");
+            System.out.println(responseEntity.getBody());
+        }else {
+            restTemplate.put(URL, employee);
+            System.out.println("Employee with id = " + id + " was updated");
+        }
     }
 
     public void deleteEmployee(int id) {
